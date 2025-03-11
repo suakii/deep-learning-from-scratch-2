b9fb8e11ecd4bd4645eedf24a2a78c7f5af2e163
Jonghwa Park <suakii@gmail.com>
2025-03-11 15:55:59 +0900

    functions

--- common/functions.py
+++ common/functions.py
@@ -11,6 +11,22 @@ def softmax(x):
         x = x - x.max(axis=1, keepdims=True)
         x = np.ex(x)
         x /= x.sum(axis=1, keepdims=True)
+    elif x.ndim == 1:
+        x = x - np.max(x)
+        x = np.ex(x) / np.sum(np.exp(x))
+    return x
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+    
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
         
 
 
