da254582c00e23af51d766eb5dba125f3d251f1e
Jonghwa Park <suakii@gmail.com>
2025-03-12 15:45:49 +0900

    laers

--- common/layers.py
+++ common/layers.py
@@ -59,12 +59,30 @@ class Softmax:
         sumdx = np.sum(dx, axis=1, keepdims=True)
         dx -= self.out * sumdx
         return dx
-    
-        
 
 
 class SoftmaxWithLoss:
-    pass
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.y = None
+        self.t = None
+    
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+
+        if self.t.tisze == self.y.size:
+            self.t = self.t.argmax(axis=1)
+        loss = cross_entropy_error(self.y, self.t)
+        return loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = self.y.copy()
+        dx[np.arange(batch_size), self.t] -= 1
+        dx *= dout
+        dx = dx / batch_size
+        return dx
 
 
 class Sigmoid:
